e1a632dfc939c43cada6967afc8cdc7d9e6ddad2
guilhermerossi1 <guilhermerossialves@gmail.com>
2026-08-12 21:53:13 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,18 @@
+
+
+
+comida = "Açaí"
+quantidadeIrmãos = 2
+preçoItemDesejado = 8.900
+animalEstimação = False
+
+
+print (comida)
+print (quantidadeIrmãos)
+print (preçoItemDesejado)
+print (animalEstimação)
+
+print (type (comida))
+print (type (quantidadeIrmãos))
+print (type (preçoItemDesejado))
+print (type (animalEstimação))
